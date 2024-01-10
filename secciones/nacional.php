624da3aab3b1ba6dd134b6fc0a97780aaf005ffd
angelGala1 <100500012+angelGala1@users.noreply.github.com>
2024-01-09 22:12:20 -0500

    Update nacional.php

--- secciones/nacional.php
+++ secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Investigacion cientifica",
+"autor" => "Renzo alvarez",
+"resumen" => "Biga data y los nuevos cambios en la tecnologia",
 ];
 ?>
