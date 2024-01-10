e244422f056936008b7122c59d58477da931ef1e
angelGala1 <100500012+angelGala1@users.noreply.github.com>
2024-01-09 22:18:29 -0500

    Update tecnologia.php

--- secciones/tecnologia.php
+++ secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Revista tecnologica",
+"autor" => "Josue sanchez",
+"resumen" => "Trata sobre la investigacion de brazos robots en la UNS",
 ];
 ?>
