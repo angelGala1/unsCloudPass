96616b721c6d785b3e469ff35297d9c08cd6a5b0
angelGala1 <100500012+angelGala1@users.noreply.github.com>
2024-01-09 22:28:02 -0500

    Update portada.php

--- secciones/portada.php
+++ secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "REVISTA UNS 2024",
+"titulo" => "REVISTA UNS 2024-INGENIERIA DE SISTEMAS",
 "autor" => "uns",
 "resumen" => "Universidad Nacional del Santa",
 ];
