c9869c2efb47cb69a5b85b0ad20c49aab95c8698
angelGala1 <100500012+angelGala1@users.noreply.github.com>
2024-01-09 22:11:45 -0500

    Update internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Revistas de investigacion",
+"autor" => "Angel Gala",
+"resumen" => "Trata sobre la investigacion de aplicaciones moviles  y de la nube",
 ];
 ?>
